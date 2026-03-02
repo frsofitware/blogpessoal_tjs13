c1219b5f595503fe6cee13ed7c8b8daa7f873fa1
Sofia Sabrina Silva <sofiasabrinasilva@gmail.com>
2026-03-02 20:41:41 -0300

    ✨ New Feature: Construção do Recurso Postagem

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,24 @@
+import { Transform, TransformFnParams } from "class-transformer";
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity({name: "tb_postagens"}) // CREATE TABLE tb_postagens
+export class Postagem{
+    
+    @PrimaryGeneratedColumn() // PRIMARY KEY (id) AUTO_INCREMENT
+    id: number;
+     
+    @Transform(({ value }: TransformFnParams) => value?.trim()) // RETIRA O ESPAÇO EM BRACO DO COMEÇO E DO FINAL
+    @IsNotEmpty() // FORÇA DIGITAÇÃO, NÃO ACEITA O CAMPO EM BRANCO
+    @Column({length: 100, nullable: false}) // VARCHAR(100) NOT NULL
+    titulo: string;
+
+    @Transform(({ value }: TransformFnParams) => value?.trim()) // RETIRA O ESPAÇO EM BRACO DO COMEÇO E DO FINAL
+    @IsNotEmpty() // FORÇA DIGITAÇÃO, NÃO ACEITA O CAMPO EM BRANCO
+    @Column({length: 1000, nullable: false}) // VARCHAR(1000) NOT NULL
+    texto: string;
+
+    @UpdateDateColumn()
+    data: Date;
+
+}
