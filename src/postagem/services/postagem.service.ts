3c5cbfd09d3ce2370509a4ba2c4c0e8dfcf39c0a
Sofia Sabrina Silva <sofiasabrinasilva@gmail.com>
2026-03-09 09:29:18 -0300

    👌 IMPROVE:  Correção de Relacionamento Entre Postagem e Tema

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -2,13 +2,15 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../entities/postagem.entity";
 import { DeleteResult, ILike, Repository } from "typeorm";
+import { TemaService } from "../../tema/services/tema.service";
 
 @Injectable()
 export class PostagemService{
 
     constructor(
         @InjectRepository(Postagem)
-        private postagemRepository : Repository <Postagem>,){}
+        private postagemRepository : Repository <Postagem>,
+        private readonly temaService: TemaService){}
 
     async findAll(): Promise<Postagem[]> {
         // SELECT * FROM tb_postagens
@@ -20,6 +22,7 @@ export class PostagemService{
     }
 
     async findById(id:number): Promise<Postagem>{
+        
         // SELECT * FROM tb_postagens WHERE id = ?
         const postagem = await this.postagemRepository.findOne({
             where: {
@@ -30,9 +33,10 @@ export class PostagemService{
             }
         })
 
-        if(!postagem)
+        if(!postagem){
             throw new HttpException("Postagem não encontrada!", HttpStatus.NOT_FOUND);
-
+        }
+        
         return postagem;
     }
 
@@ -49,18 +53,27 @@ export class PostagemService{
     }
 
     async create(postagem: Postagem): Promise<Postagem>{
-        // INSEERT INTO tb_postagens (titulo, texto) VALUES
+
+        await this.temaService.findById(postagem.tema.id);
+
+        // INSERT INTO tb_postagens (titulo, texto) VALUES
         return await this.postagemRepository.save(postagem);
     }
 
     async update(postagem: Postagem): Promise<Postagem>{
+        
         // UPDATE tb_postagens SET titulo = ?,  texto = ?, data = CURRENT_TIMESTAMP();
         // WHERE id = ?;
 
         if(!postagem.id || postagem.id <= 0)
             throw new HttpException("O ID da postagem é inválido!", HttpStatus.BAD_REQUEST);
 
+        // CHECA SE A POSTAGEM EXISTE
         await this.findById(postagem.id);
+
+        //CHECA SE O TEMA DA POSTAGEM EXISTE
+        await this.temaService.findById(postagem.tema.id);
+        
         return this.postagemRepository.save(postagem);
     }
 
