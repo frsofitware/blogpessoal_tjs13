b5984f730ae8fafc5146fe74f2f3b2b5b31deb79
sofia <sofiasabrinasilva@gmail.com>
2026-03-17 00:51:41 -0300

    🚀 Deploy:  Deploy do Projeto Blog Pessoal.

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -3,6 +3,7 @@ import { IsNotEmpty, Length } from 'class-validator';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { Tema } from '../../tema/entities/tema.entity';
 import { Usuario } from '../../usuario/entities/usuario.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_postagens' }) // CREATE TABLE tb_postagens
 export class Postagem {
@@ -13,22 +14,27 @@ export class Postagem {
     @IsNotEmpty({ message: 'O Título é Obrigatório' }) // Forçar digitação
     @Length(5, 100, { message: 'O Título deve ter entre 5 e 100 caracteres' })
     @Column({ length: 100, nullable: false }) // VARCHAR(100) NOT NULL
+    @ApiProperty()  
     titulo: string;
 
     @Transform(({ value }: TransformFnParams) => value?.trim()) // Remover espaços em branco I/F
     @IsNotEmpty({ message: 'O Texto é Obrigatório' }) // Forçar digitação
     @Length(10, 1000, { message: 'O Texto deve ter entre 10 e 1000 caracteres' })
     @Column({ length: 1000, nullable: false }) // VARCHAR(1000) NOT NULL
+    @ApiProperty()
     texto: string;
 
     @UpdateDateColumn() // Atualiza a data na criação e na atualização
+    @ApiProperty()
     data: Date; // DATETIME(6)
 
+    @ApiProperty({type: () => Tema})
     @ManyToOne( () => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE"
     })
     tema: Tema; // Representa a Chave Estrangeira
 
+    @ApiProperty({type: ()=> Usuario})
     @ManyToOne( () => Usuario, (usuario) => usuario.postagem, {
         onDelete: "CASCADE"
     })
